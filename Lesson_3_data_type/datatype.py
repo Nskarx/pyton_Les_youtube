0814c284248e51756466a7e9aef4eabe5ae7e42d
Evgeniy <Lymilight@gmail.com>
2023-04-11 20:55:14 +0700

    les3

---
+++ Lesson_3_data_type/datatype.py
@@ -0,0 +1,46 @@
+# типы данных
+
+# 1. int -целое число
+x = 1
+a = -123
+
+# 2. float - число с плавающей точкой
+z = 1.5
+
+# 3. bool -логический тип
+check = z > x
+print(check) # True
+
+# 4. str -строка
+message = 'Hello'
+phrase = "World"
+print("i'm happy")
+
+message = 'Hello \n World' # перенос на другую строку
+print(message)
+
+message = """ Hello 
+ World!!!
+"""
+print(message)
+
+# 5. None в обозначаем что в сылке нет ничего
+print(type(None))
+
+
+print(type('100')) # str
+
+print(type(123 < 4566)) # bool
+
+print(type("123 < 4566")) # str
+
+print(type("True")) # str
+
+print(type(True)) # bool
+
+print(type(1.5 + 10)) # float
+
+print(type(1 + 5)) # int
+
+
+
