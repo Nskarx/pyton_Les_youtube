89ba518b15dde815f8fa1e9910f7eae6bdd671d9
Evgeniy <Lymilight@gmail.com>
2023-04-23 21:42:56 +0700

    les4

--- lesson_4_conversion/conversion.py
+++ lesson_4_conversion/conversion.py
@@ -32,3 +32,26 @@ print(int('10') + 10)
 print(str(int('10')) + str(10))  # 1010 -это конкатанация
 
 
+print(bool(1000)) # True
+print(bool(-1000)) # True  в данных строках преобразует числа в решения
+print(bool(0)) # False
+print(bool('hello')) # True
+print(bool('')) # False   в данных строках преобразует строки
+print(bool(' ')) # True
+print(bool(None)) # False
+
+int(str(int(str(10)))) + int(str(5))
+
+print(int(str(int(str(10)))) + int(str(5)))
+
+
+print(not bool('hello')) # False
+
+bool(10 % 3)
+print(bool(10 % 3)) # True
+
+bool('' + '')
+print(bool('' + ''))  # False
+
+# bool(input('enter the number: '))
+# print(bool(input('enter the number: '))) # True
